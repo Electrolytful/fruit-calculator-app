f74d83c181f1947d066240476b6a7a5548598489
Electrolytful <erhanulusal@hotmail.com>
2023-02-27 22:08:49 +0000

    added image add and remove feature and started on css

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fruit Calorie Calculator
+
+### Work in progress of a fruits calculator app that adds the fruits the user inputs into a list and sums up the calories of those fruits.
+
+### To run open up terminal and navigate to the app directory, then run 'npm run dev' and use live server on index.html file to view page.
+
+### Work to be done:
+1. Complete CSS styling of the page to make it neat
+2. Add more functionality in future e.g. quantity selector and other foods
+3. Get better quality image API - maybe PNG icons
--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const { getEventListeners } = require("events");
 
+// selecting DOM elements to add content to dynamically 
 const fruitForm = document.querySelector("#inputSection form");
 const fruitList = document.querySelector("#fruitSection ul");
-const fruitNutrition = document.querySelector("#nutritionSection p");
-const APIkey = "33986188-7c2c501da5f77845a5bf70b41";
+const fruitNutrition = document.querySelector("#calorieCount");
+const fruitImage = document.querySelector("#imageSection");
 
-fruitForm.addEventListener("submit", extractFruit);
-
+// global variables
+const APIkey = "33986188-7c2c501da5f77845a5bf70b41";     //SHOULD BE KEPT SECRET
 let calorie = 0;
 
+// event listener for the submit button calling the extract fruit function (see below)
+fruitForm.addEventListener("submit", extractFruit);
+
+// function checking to see whether a valid input is submitted then calling the fetch function below
 function extractFruit(e)  {
     e.preventDefault();
     let fruitInput = e.target.fruitInput.value;
@@ -19,13 +24,15 @@ function extractFruit(e)  {
     e.target.reset();
 };
 
-// asynchronous function to get the fruit data from API when user hits submit
+// asynchronous function to get the fruit data from API when user hits submit, the data is then passed into the addFruit function below
 async function fetchFruitData(fruit) {
     try {
         const resp = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`);
-        if(resp.ok) {
-            const data = await resp.json();
-            addFruit(data);
+        const resp2 = await fetch(`https://pixabay.com/api/?key=${APIkey}&q=${fruit}+fruit`);
+        if(resp.ok && resp2.ok) {
+            const fruitData = await resp.json();
+            const imgData = await resp2.json();
+            addFruit(fruitData, imgData);
         } else {
             throw `Error: http status code = ${resp.status}`;
         }
@@ -34,33 +41,38 @@ async function fetchFruitData(fruit) {
     }
 }
 
-function addFruit(fruit) {
+// function to get fruit and image data, creating a list and image element, as well as updating the total calorie count using the calorieCount function (add and remove)s
+function addFruit(fruitData, imgData) {
     // create list item
     const li = document.createElement('li');
-    // assign text to list item
-    li.textContent = fruit.name;
-    // append list item to the htmll list
+    const img = document.createElement('img');
+    // assign text to list item and image data to the image src
+    li.textContent = fruitData.name;
+    img.src = imgData.hits[Math.floor(Math.random() * imgData.hits.length)].largeImageURL;
+    img.alt = fruitData;
+    img.height = 250;
+    img.width = 250;
+    // append list and image item to the DOM
     fruitList.appendChild(li);
+    fruitImage.appendChild(img);
     // update calorie count depending on fruit added
-    fruitNutrition.textContent = calorieCount(fruit, "add");
-    // add event listener to remove fruit item when clicked
+    fruitNutrition.textContent = calorieCount(fruitData, "add");
+    // add event listener to remove fruit item and image connected to that fruit item when clicked
     li.addEventListener("click", (e) => {
-        removeFruit(e);
-        fruitNutrition.textContent = calorieCount(fruit, "remove");
+        e.target.remove();
+        img.remove();
+        fruitNutrition.textContent = calorieCount(fruitData, "remove");
     }, {once: true});
 };
 
-function removeFruit(e) {
-    e.target.remove();
-}
-
+// function to malipulate the calorie count when fruit is added and removed, takes two params: fruit = the fruit item(data from API), option: either += calories or -= calories
 function calorieCount(fruit, option) {
     if(option === "add") {
         calorie += fruit.nutritions.calories;
-        return `Total calorie count = ${calorie}`;
+        return `${calorie}`;
     }
     if(option === "remove") {
         calorie -= fruit.nutritions.calories;
-        return `Total calorie count = ${calorie}`;
+        return `${calorie}`;
     }
 }
